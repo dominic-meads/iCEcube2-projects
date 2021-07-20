bca54edd8b2de6e9f85e5b48c0f3b4dbde0d693f
Dominic Meads <56456214+dominic-meads@users.noreply.github.com>
2021-07-19 21:39:23 -0700

    removed rst, added pattern 1 logic

--- LED_patterns/src/LED_patterns.vhd
+++ LED_patterns/src/LED_patterns.vhd
@@ -27,7 +27,6 @@ use IEEE.numeric_std.all;
 entity LED_patterns is 
   port (
     i_clk  : in  std_logic;  -- 12 MHz
-    i_rst  : in  std_logic;
     o_LEDs : out std_logic_vector(4 downto 0)  
     );
 end LED_patterns;
@@ -35,10 +34,13 @@ end LED_patterns;
 architecture rtl of LED_patterns is
 
   -- enables for different patterns/counters
-  signal r_pattern1_en : std_logic := '0';
-  signal r_pattern2_en : std_logic := '0';
-  signal r_pattern3_en : std_logic := '0';
-  signal r_pattern4_en : std_logic := '0';
+  signal r_patt1_cntr_en : std_logic := '0';
+  signal r_patt2_cntr_en : std_logic := '0';
+  signal r_patt3_cntr_en : std_logic := '0';
+  signal r_patt4_cntr_en : std_logic := '0';
+  
+  -- pattern counters 
+  signal r_patt1_cntr : integer range 0 to 750e3 := 0;
   
   -- indicates if pattern should be changed
   signal r_change_pattern : std_logic := '0';
@@ -53,22 +55,42 @@ architecture rtl of LED_patterns is
 
 begin 
 
-  DISP_PATT_PROC : process(i_clk, i_rst)
+  -- display each pattern for 2 seconds (12 MHz clk * 2 = 24M counts)
+  DISP_PATT_PROC : process(i_clk)
   begin 
     if rising_edge(i_clk) then 
-      if i_rst = '1' then 
-        r_change_pattern <= '0';
-        r_clk_cntr <= 0;
+      if r_clk_cntr < 24e6 then 
+        r_clk_cntr <= r_clk_cntr + 1;
       else 
-        if r_clk_cntr < 24e6 then 
-          r_clk_cntr <= r_clk_cntr + 1;
-          r_change_pattern <= '0';
+        r_clk_cntr <= 0;
+      end if;
+    end if;
+  end process;
+  
+  -- activate the "r_change_pattern" flag every 2 seconds
+  r_change_pattern <= '1' when r_clk_cntr = 24e6 else '0';
+  
+  -- pattern 1 counter switches LEDs every 62.5 ms
+  PATT1_CNTR_PROC : process(i_clk, r_patt1_cntr_en)
+  begin 
+    if rising_edge(i_clk) then 
+      if r_patt1_cntr_en = '0' then 
+        r_patt1_cntr <= 0;
+      else 
+        if r_patt1_cntr < 750e3 then 
+          r_patt1_cntr <= r_patt1_cntr + 1;
         else 
-          r_clk_cntr <= 0;
-          r_change_pattern <= '1';
+          r_patt1_cntr <= 0;
         end if;
       end if;
     end if;
   end process;
-    
-    
+  
+  
+      
+      
+  
+
+
+
+end architecture;
